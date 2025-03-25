fb495058aaa813b30310b854068559e411ea6972
Andreas Salminger <andisalminger@hotmail.com>
2025-03-25 11:51:19 +0100

    UE04: created module for Spring Boot

---
+++ UE03/projects-spring/worklog-api/src/main/java/swt6/spring/worklog/config/DatabaseInitializer.java
@@ -0,0 +1,18 @@
+package swt6.spring.worklog.config;
+
+//Employee empl1 = new Employee("Sepp", "Forcher", LocalDate.of(1935, 12, 12));
+//empl1.addLogbookEntry(new LogbookEntry("Jour Fixe", LocalDateTime.of(2018, 3, 1, 8, 15), LocalDateTime.of(2018, 3, 1, 10, 0)));
+//empl1.addLogbookEntry(new LogbookEntry("Analyse", LocalDateTime.of(2018, 3, 1, 10, 0), LocalDateTime.of(2018, 3, 1, 13, 45)));
+//empl1.addLogbookEntry(new LogbookEntry("Implementierung", LocalDateTime.of(2018, 3, 1, 10, 15), LocalDateTime.of(2018, 3, 1, 14, 30)));
+//workLog.syncEmployee(empl1);
+//
+//Employee empl2 = new Employee("Alfred", "Kunz", LocalDate.of(1944, 8, 10));
+//empl2.addLogbookEntry(new LogbookEntry("Jour Fixe", LocalDateTime.of(2018, 3, 1, 8, 15), LocalDateTime.of(2018, 3, 1, 10, 0)));
+//empl2.addLogbookEntry(new LogbookEntry("Unit-Test schreiben", LocalDateTime.of(2018, 3, 1, 10, 15), LocalDateTime.of(2018, 3, 1, 14, 30)));
+//empl2.addLogbookEntry(new LogbookEntry("Integrations-Tests wiederholen", LocalDateTime.of(2018, 3, 1, 14, 30), LocalDateTime.of(2018, 3, 1, 16, 0)));
+//workLog.syncEmployee(empl2);
+//
+//Employee empl3 = new Employee("Sigfried", "Hinz", LocalDate.of(1954, 5, 3));
+//empl3.addLogbookEntry(new LogbookEntry("Jour Fixe", LocalDateTime.of(2018, 3, 1, 8, 15), LocalDateTime.of(2018, 3, 1, 10, 0)));
+//empl3.addLogbookEntry(new LogbookEntry("Benutzerdoku aktualisieren", LocalDateTime.of(2018, 3, 1, 8, 15), LocalDateTime.of(2018, 3, 1, 16, 30)));
+//workLog.syncEmployee(empl3);
