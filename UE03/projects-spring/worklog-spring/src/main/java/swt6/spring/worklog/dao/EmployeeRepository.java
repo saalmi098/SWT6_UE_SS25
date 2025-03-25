6ba2ede07dfd8e13b3922bd71d8d9bec2f8437ad
Andreas Salminger <andisalminger@hotmail.com>
2025-03-25 09:51:17 +0100

    UE04: Spring Data - Queries

--- UE03/projects-spring/worklog-spring/src/main/java/swt6/spring/worklog/dao/EmployeeRepository.java
+++ UE03/projects-spring/worklog-spring/src/main/java/swt6/spring/worklog/dao/EmployeeRepository.java
@@ -1,10 +1,28 @@
 package swt6.spring.worklog.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swt6.spring.worklog.domain.Employee;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 // JpaRepository aus Spring Data, stellt CRUD-Methoden fuer eine Entity bereit
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // V1: Generierte Abfrage: Ableitung aus Methodennamen
+    Optional<Employee> findByLastName(String lastName);
+
+    // V2: Generierte Abfrage: Deklarativ (wuerde auch schon durch Methodennamen funktionieren)
+    // @Query nimmt eine JPQL-Query oder native SQL-Query entgegen
+//    @Query("select e from Employee e where e.lastName like %?1%") // ?1 ist der erste Parameter
+    @Query("select e from Employee e where e.lastName like %:substring%") // benannter Parameter
+    List<Employee> findByLastNameContaining(@Param("substring") String lastName);
+
+    @Query("select e from Employee e where e.dateOfBirth < :date")
+    List<Employee> findOlderThan(@Param("date") LocalDate date); // best practice: @Param immer verwenden
 }
--- UE03/projects-spring/worklog-spring/src/test/java/swt6/spring/worklog/dao/SpringDataRepositoryTest.java
+++ UE03/projects-spring/worklog-spring/src/test/java/swt6/spring/worklog/dao/SpringDataRepositoryTest.java
@@ -25,7 +25,7 @@ public class SpringDataRepositoryTest {
     @Transactional
     public void testSave() {
         Employee empl1 = new Employee("Josefine", "Feichtlbauer", LocalDate.of(1970, 10, 26));
-        Employee empl2 = new Employee("Josef", "Himmelbauer", LocalDate.of(1964, 1, 15));
+        Employee empl2 = new Employee("Josef", "Himmelbauer", LocalDate.of(1990, 1, 15));
 
         PrintUtil.printTitle("insert employee", 60, '-');
         empl1 = employeeRepository.save(empl1);
@@ -52,6 +52,17 @@ public class SpringDataRepositoryTest {
     }
 
     @Test
+    @Transactional
     public void testQueries() {
+        testSave();
+
+        PrintUtil.printTitle("findByLastName", 60, '-');
+        Optional<Employee> empl = employeeRepository.findByLastName("Himmelbauer-Huber");
+
+        PrintUtil.printTitle("findByLastNameContaining", 60, '-');
+        employeeRepository.findByLastNameContaining("mm").forEach(System.out::println);
+
+        PrintUtil.printTitle("findOlderThan", 60, '-');
+        employeeRepository.findOlderThan(LocalDate.of(1980, 1, 1)).forEach(System.out::println);
     }
 }
